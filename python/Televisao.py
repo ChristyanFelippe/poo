f4373e9f563bf2c652d42802f29e4a8bb8e50f37
ch052877 <christyanmend@gmail.com>
2023-12-03 04:03:05 -0300

    criando teste com construtores

--- python/Televisao.py
+++ python/Televisao.py
@@ -1,14 +1,23 @@
 class Televisao():
-    def __init__(self, marca, modelo, preco, ano):
+    def __init__(self, marca, n_canais, preco, ano):
         print("A tv {} foi criada".format(self))
         self.marca = marca
-        self.modelo = modelo
+        self.n_canais = n_canais
         self.preco = preco
         self.ano = ano
+        
+    def aumenta_canal(self):
+        self.n_canais += 1
+        
+    def diminui_canal(self):
+        self.n_canais -= 1
 
 
-t1 = Televisao("LG", "I3031", 1900, 2018)
+t1 = Televisao("LG", 40, 1900, 2018)
 print(t1.marca)
-print(t1.modelo)
+print(t1.n_canais)
 print(t1.preco)
 print(t1.ano)
+
+t1.aumenta_canal()
+print(t1.n_canais)
