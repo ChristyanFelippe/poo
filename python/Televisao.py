95946a26dd4c3f18ef64255573238aa8eba40f76
ch052877 <christyanmend@gmail.com>
2023-12-04 00:23:42 -0300

    adicionando encapsulamento

--- python/Televisao.py
+++ python/Televisao.py
@@ -5,20 +5,18 @@ class Televisao():
         self.n_canais = n_canais
         self.preco = preco
         self.ano = ano
+        self._ligada = False
         
     def aumenta_canal(self):
         self.n_canais += 1
         
     def diminui_canal(self):
         self.n_canais -= 1
-
-
+        
+    def liga_tv(self):
+        self._ligada = True
+        
 t1 = Televisao("LG", 40, 1900, 2018)
-print(t1.marca)
-print(t1.n_canais)
-print(t1.preco)
-print(t1.ano)
-
 t1.aumenta_canal()
 print(t1.n_canais)
 
@@ -32,7 +30,13 @@ print(t1)
 tv_nova = t1
 print(tv_nova)
 
-
 tv_nova = None
-
 print(tv_nova)
+
+## Encapsulamento a partir daqui
+
+## Atributos privados
+
+print(t1.ligada)
+t1.liga_tv()
+print(t1.ligada)
